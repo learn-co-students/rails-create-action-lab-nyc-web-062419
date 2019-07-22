9516ef1cbdade970c08f0dc57eff9df271cc095c
Anthony <anthonywan00@gmail.com>
2019-07-22 10:35:41 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,17 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student =  Student.new(first_name: params[:first_name], last_name: params[:last_name])
+    if @student.valid?
+      @student.save
+      redirect_to @student
+    else 
+      render :new
+    end
   end
 
 end
